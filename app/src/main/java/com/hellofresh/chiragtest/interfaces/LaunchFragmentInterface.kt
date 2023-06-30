27a33e59bd997e4378d93a1642dc4e5f3993c078
chirag khandelwal <chiragkhandelwal@chirags-MacBook-Pro.local>
2023-06-30 21:35:25 +0530

    first commit

--- app/src/main/java/com/hellofresh/chiragtest/interfaces/LaunchFragmentInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.hellofresh.chiragtest.interfaces
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-
-interface LaunchFragmentInterface {
-    fun launchFragment(bundle: Bundle? = null, fragment: Fragment? = null)
-}
